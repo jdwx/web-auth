eca1c0268d546e116d731bddd5e2ccbffcb320ef
JDW <xs@jdw.sx>
2025-08-12 23:01:52 -0400

    Fix login redirect.

--- src/Routes/UserRoute.php
+++ src/Routes/UserRoute.php
@@ -19,33 +19,15 @@ class UserRoute extends AuthRoute {
     protected const Level REQUIRED_LEVEL = Level::USER;
 
 
-    protected function handleGET( string $i_stUri, string $i_stPath, array $i_rUriParameters ) : ?ResponseInterface {
-        if ( $this->isLoggedIn() ) {
-            return $this->loggedInGet( $i_stUri, $i_stPath, $i_rUriParameters );
-        }
-        return $this->loggedOutGet( $i_stUri, $i_stPath, $i_rUriParameters );
-    }
-
-
     /**
      * @param array<string, string|list<string>> $i_rUriParameters
-     * @noinspection PhpUnusedParameterInspection
      */
-    protected function loggedInGet( string $i_stUri, string $i_stPath, array $i_rUriParameters ) : ?ResponseInterface {
-        return parent::handleGET( $i_stUri, $i_stPath, $i_rUriParameters );
-    }
-
-
-    /**
-     * @param array<string, string|list<string>> $i_rUriParameters
-     * @noinspection PhpUnusedParameterInspection
-     */
-    protected function loggedOutGet( string $i_stUri, string $i_stPath, array $i_rUriParameters ) : ?ResponseInterface {
+    protected function forbidden( string $i_stUri, string $i_stPath, array $i_rUriParameters ) : ?ResponseInterface {
         $nstLoginUri = $this->getTagUri( RouteTag::LOGIN );
         if ( is_string( $nstLoginUri ) ) {
             return Response::redirectTemporaryWithGet( $nstLoginUri );
         }
-        $this->forbidden( $i_stUri, $i_stPath );
+        return parent::forbidden( $i_stUri, $i_stPath, $i_rUriParameters );
     }
 
 
